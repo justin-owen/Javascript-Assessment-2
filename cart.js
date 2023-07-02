84724cee53f3d22e1b1979f906e11ea534d783bb
justin-owen <justinowen22345@gmail.com>
2023-07-01 23:37:59 -0400

    Assessment Complete

---
+++ cart.js
@@ -0,0 +1,116 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+const summedPrice = cart.reduce((acc, cur) => acc + cur.price,0)
+console.log(summedPrice)
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+//CODE HERE
+const calcFinalPrice = (cartTotal, couponValue, tax) => {
+    let taxedItem = cartTotal + (cartTotal * tax)
+    let finalCost = taxedItem - couponValue
+    return finalCost
+}
+console.log(calcFinalPrice(summedPrice, 3, .06))
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+    TEXT ANSWER HERE
+    -The customer object should contain the name of the customer 
+    which will be a string to identify the specific customer and it 
+    is a string because it contains letters and so that we can see the full name and be able to modify it if needed.
+    - The second property will be the customer email in order to contact the customer
+    to send them the reciept or any deals the restuarant provides and this will
+    be a string as well because it contains letters and so that we can display it or modify it if needed.
+    - The third property would be the billing address so that we could store their information
+    and reference back to it if need be for records. This will be a string because it 
+    would contain letters.
+    - The fourth property will be the order number so we can reference the order that
+    the customer made and this will be a number so that we can both reference it and increment each order
+    so they will be unique and in order.
+    The fifth property will be paid which will be a boolean so we can track whether they paid or not.
+
+
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+//CODE HERE
+const customers = {
+    name: "Justin Owen",
+    email: "justin.owen@email.com",
+    Billing_address: "23 Road Way City, State 11111",
+    order: 23,
+    paid: true
+}
